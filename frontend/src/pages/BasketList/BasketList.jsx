92221afc8cf75ffc00d4072c5e7d3d198f77be78
YakomazovPavel <yakomazov.pavel@yandex.ru>
2025-08-13 23:58:02 +0000

    add pages

---
+++ frontend/src/pages/BasketList/BasketList.jsx
@@ -0,0 +1,138 @@
+function BasketList() {
+  return (
+    <div id="page_basket_list">
+      <div className="settings_wrap">
+        {/* <!-- Сюда добавить поисковую строку --> */}
+        <div className="basket_list_section">
+          <input className="hide" type="checkbox" name="my_basket" value="value" id="my_basket_section" />
+          <label htmlFor="my_basket_section">
+            <p>Мои корзины</p>
+            <svg className="arrow_down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129">
+              <use xlinkHref="#arrow"></use>
+            </svg>
+          </label>
+          <div className="basket_list_item">
+            <img src="1.jpg" />
+            <div>
+              <h1>Посиделки 2 ноября</h1>
+              <h2>#1</h2>
+            </div>
+            <button>
+              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <use xlinkHref="#circle_arrow"></use>
+              </svg>
+            </button>
+          </div>
+          <div className="basket_list_item">
+            <img src="2.jpg" />
+            <div>
+              <h1>Хеллоуин</h1>
+              <h2>#2</h2>
+            </div>
+            <button>
+              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <use xlinkHref="#circle_arrow"></use>
+              </svg>
+            </button>
+          </div>
+          <div className="basket_list_item">
+            <img src="3.jpg" />
+            <div>
+              <h1>Тыквенный спас</h1>
+              <h2>#3</h2>
+            </div>
+            <button>
+              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <use xlinkHref="#circle_arrow"></use>
+              </svg>
+            </button>
+          </div>
+          <div className="basket_list_item">
+            <img src="9.jpg" />
+            <div>
+              <h1>Отчаяние</h1>
+              <h2>#19</h2>
+            </div>
+            <button>
+              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <use xlinkHref="#circle_arrow"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="basket_list_section">
+          <input className="hide" type="checkbox" name="my_basket" value="value" id="not_my_basket_section" />
+          <label htmlFor="not_my_basket_section">
+            <p>Доступные мне</p>
+            <svg className="arrow_down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129">
+              <use xlinkHref="#arrow"></use>
+            </svg>
+          </label>
+          <div className="basket_list_item">
+            <img src="4.jpg" />
+            <div>
+              <h1>Проводы</h1>
+              <h2>#12</h2>
+            </div>
+            <button>
+              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <use xlinkHref="#circle_arrow"></use>
+              </svg>
+            </button>
+          </div>
+          <div className="basket_list_item">
+            <img src="5.jpg" />
+            <div>
+              <h1>Свадьба</h1>
+              <h2>#15</h2>
+            </div>
+            <button>
+              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <use xlinkHref="#circle_arrow"></use>
+              </svg>
+            </button>
+          </div>
+          <div className="basket_list_item">
+            <img src="6.jpg" />
+            <div>
+              <h1>Посиделки 12.05</h1>
+              <h2>#72</h2>
+            </div>
+            <button>
+              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <use xlinkHref="#circle_arrow"></use>
+              </svg>
+            </button>
+          </div>
+          <div className="basket_list_item">
+            <img src="7.jpg" />
+            <div>
+              <h1>Посиделки маглов</h1>
+              <h2>#82</h2>
+            </div>
+            <button>
+              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <use xlinkHref="#circle_arrow"></use>
+              </svg>
+            </button>
+          </div>
+          <div className="basket_list_item">
+            <img src="8.jpg" />
+            <div>
+              <h1>Посиделки без повода</h1>
+              <h2>#17</h2>
+            </div>
+            <button>
+              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <use xlinkHref="#circle_arrow"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BasketList;
---
+++ frontend/src/pages/CreateBasket/CreateBasket.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function CreateBasket() {
+  return (
+    <div id="page_basket_create">
+      <div className="create_basket_header">
+        <img src="icon2.jpg" alt="" className="icon" />
+        <div className="main_title_wrap">
+          <h1 className="main_title">VAFFEL</h1>
+          <h2 className="main_subtitle" id="main_subtitle">
+            Поделись корзиной вафель
+          </h2>
+        </div>
+      </div>
+
+      <div className="settings_wrap">
+        <form className="settings" name="form_create_basket" action="./">
+          <div className="settings_item">
+            <input id="input_basket_name" type="text" placeholder="Название корзины" name="basket_name" />
+          </div>
+          <div className="settings_item">
+            <p>Дата окончания</p>
+            <input className="input_basket_datetime" id="input_basket_date" type="date" name="basket_start" />
+          </div>
+          <div className="settings_item">
+            <p>Время окончания</p>
+            <input className="input_basket_datetime" id="input_basket_time" type="time" name="basket_end" />
+          </div>
+        </form>
+        <button id="to_baskets_button">
+          <p>К корзинам</p>
+          <svg className="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129">
+            <use xlinkHref="#arrow"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CreateBasket;
