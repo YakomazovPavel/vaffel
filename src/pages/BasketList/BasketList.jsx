c0a079c444e306e43e0af12d1e10ec1213046a6d
YakomazovPavel <yakomazov.pavel@yandex.ru>
2025-08-19 18:52:17 +0300

    1

--- src/pages/BasketList/BasketList.jsx
+++ src/pages/BasketList/BasketList.jsx
@@ -2,48 +2,62 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setCurrentPage, PAGE } from "../../slices/appSlice.js";
 
-var basketListData = [
-  {
-    id: "1",
-    photo_url: "1.jpg",
-    author_id: "1",
-    name: "Посиделки на свадьбе",
-    is_locked: false,
-    created: "2025-08-19T14:52:54.348Z",
-    updated: "2025-08-19T14:52:54.348Z",
-  },
-  {
-    id: "2",
-    photo_url: "1.jpg",
-    author_id: "1",
-    name: "Посиделки на похоронах",
-    is_locked: false,
-    created: "2025-08-19T14:52:54.348Z",
-    updated: "2025-08-19T14:52:54.348Z",
-  },
-  {
-    id: "3",
-    photo_url: "1.jpg",
-    author_id: "2",
-    name: "Чаяпитие",
-    is_locked: false,
-    created: "2025-08-19T14:52:54.348Z",
-    updated: "2025-08-19T14:52:54.348Z",
-  },
-  {
-    id: "4",
-    photo_url: "1.jpg",
-    author_id: "3",
-    name: "Тимбилдинг 18:00",
-    is_locked: false,
-    created: "2025-08-19T14:52:54.348Z",
-    updated: "2025-08-19T14:52:54.348Z",
-  },
-];
+const mockBasketListData = {
+  my: [
+    {
+      id: "1",
+      photo_url: "1.jpg",
+      author_id: "1",
+      name: "Посиделки на свадьбе",
+      is_locked: false,
+      created: "2025-08-19T14:52:54.348Z",
+      updated: "2025-08-19T14:52:54.348Z",
+    },
+    {
+      id: "5",
+      photo_url: "5.jpg",
+      author_id: "1",
+      name: "Первая карзина",
+      is_locked: false,
+      created: "2025-08-19T14:52:54.348Z",
+      updated: "2025-08-19T14:52:54.348Z",
+    },
+  ],
+  other: [
+    {
+      id: "2",
+      photo_url: "2.jpg",
+      author_id: "1",
+      name: "Посиделки на похоронах",
+      is_locked: false,
+      created: "2025-08-19T14:52:54.348Z",
+      updated: "2025-08-19T14:52:54.348Z",
+    },
+    {
+      id: "3",
+      photo_url: "3.jpg",
+      author_id: "2",
+      name: "Чаяпитие",
+      is_locked: false,
+      created: "2025-08-19T14:52:54.348Z",
+      updated: "2025-08-19T14:52:54.348Z",
+    },
+    {
+      id: "4",
+      photo_url: "4.jpg",
+      author_id: "3",
+      name: "Тимбилдинг 18:00",
+      is_locked: false,
+      created: "2025-08-19T14:52:54.348Z",
+      updated: "2025-08-19T14:52:54.348Z",
+    },
+  ],
+};
 
 function BasketList() {
   const dispatch = useDispatch();
-  const [dataLoading, setDataLoading] = useState(true);
+  // const [dataLoading, setDataLoading] = useState(true);
+  const [basketListData, setBasketListData] = useState(mockBasketListData);
 
   var backButtonHandler = () => {
     dispatch(setCurrentPage(PAGE.CreateBasket));
@@ -79,63 +93,31 @@ function BasketList() {
     <div id="page_basket_list">
       <div className="settings_wrap">
         {/* <!-- Сюда добавить поисковую строку --> */}
-        <div className="basket_list_section">
-          <input className="hide" type="checkbox" name="my_basket" value="value" id="my_basket_section" />
-          <label htmlFor="my_basket_section" className="basket_section_label">
-            <p>Мои корзины</p>
-            <svg className="arrow_down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129">
-              <use xlinkHref="#arrow"></use>
-            </svg>
-          </label>
-          <div className="basket_list_item">
-            <img src="1.jpg" />
-            <div>
-              <h1>Посиделки 2 ноября</h1>
-              <h2>#1</h2>
-            </div>
-            <button>
-              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                <use xlinkHref="#circle_arrow"></use>
+        {basketListData?.my && (
+          <div className="basket_list_section">
+            <input className="hide" type="checkbox" name="my_basket" value="value" id="my_basket_section" />
+            <label htmlFor="my_basket_section" className="basket_section_label">
+              <p>Мои корзины</p>
+              <svg className="arrow_down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129">
+                <use xlinkHref="#arrow"></use>
               </svg>
-            </button>
+            </label>
+
+            {basketListData.my.map((basket) => (
+              <div className="basket_list_item">
+                <img src={basket.photo_url} />
+                <div>
+                  <h1>{basket.name}</h1>
+                </div>
+                <button>
+                  <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                    <use xlinkHref="#circle_arrow"></use>
+                  </svg>
+                </button>
+              </div>
+            ))}
           </div>
-          <div className="basket_list_item">
-            <img src="2.jpg" />
-            <div>
-              <h1>Хеллоуин</h1>
-              <h2>#2</h2>
-            </div>
-            <button>
-              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                <use xlinkHref="#circle_arrow"></use>
-              </svg>
-            </button>
-          </div>
-          <div className="basket_list_item">
-            <img src="3.jpg" />
-            <div>
-              <h1>Тыквенный спас</h1>
-              <h2>#3</h2>
-            </div>
-            <button>
-              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                <use xlinkHref="#circle_arrow"></use>
-              </svg>
-            </button>
-          </div>
-          <div className="basket_list_item">
-            <img src="9.jpg" />
-            <div>
-              <h1>Отчаяние</h1>
-              <h2>#19</h2>
-            </div>
-            <button>
-              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                <use xlinkHref="#circle_arrow"></use>
-              </svg>
-            </button>
-          </div>
-        </div>
+        )}
 
         <div className="basket_list_section">
           <input className="hide" type="checkbox" name="my_basket" value="value" id="not_my_basket_section" />
