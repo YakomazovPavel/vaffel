33beaa1900fc914be18d04d850d6aec471bad5d6
YakomazovPavel <yakomazov.pavel@yandex.ru>
2025-08-19 18:42:34 +0300

    1

--- src/pages/BasketList/BasketList.jsx
+++ src/pages/BasketList/BasketList.jsx
@@ -1,9 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setCurrentPage, PAGE } from "../../slices/appSlice.js";
 
+var basketListData = [
+  {
+    id: "1",
+    photo_url: "1.jpg",
+    author_id: "1",
+    name: "Посиделки на свадьбе",
+    is_locked: false,
+    created: "2025-08-19T14:52:54.348Z",
+    updated: "2025-08-19T14:52:54.348Z",
+  },
+  {
+    id: "2",
+    photo_url: "1.jpg",
+    author_id: "1",
+    name: "Посиделки на похоронах",
+    is_locked: false,
+    created: "2025-08-19T14:52:54.348Z",
+    updated: "2025-08-19T14:52:54.348Z",
+  },
+  {
+    id: "3",
+    photo_url: "1.jpg",
+    author_id: "2",
+    name: "Чаяпитие",
+    is_locked: false,
+    created: "2025-08-19T14:52:54.348Z",
+    updated: "2025-08-19T14:52:54.348Z",
+  },
+  {
+    id: "4",
+    photo_url: "1.jpg",
+    author_id: "3",
+    name: "Тимбилдинг 18:00",
+    is_locked: false,
+    created: "2025-08-19T14:52:54.348Z",
+    updated: "2025-08-19T14:52:54.348Z",
+  },
+];
+
 function BasketList() {
   const dispatch = useDispatch();
+  const [dataLoading, setDataLoading] = useState(true);
 
   var backButtonHandler = () => {
     dispatch(setCurrentPage(PAGE.CreateBasket));
@@ -31,6 +71,7 @@ function BasketList() {
 
   useEffect(() => {
     open();
+    load;
     return close;
   }, []);
 
@@ -40,7 +81,7 @@ function BasketList() {
         {/* <!-- Сюда добавить поисковую строку --> */}
         <div className="basket_list_section">
           <input className="hide" type="checkbox" name="my_basket" value="value" id="my_basket_section" />
-          <label htmlFor="my_basket_section">
+          <label htmlFor="my_basket_section" className="basket_section_label">
             <p>Мои корзины</p>
             <svg className="arrow_down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129">
               <use xlinkHref="#arrow"></use>
@@ -98,7 +139,7 @@ function BasketList() {
 
         <div className="basket_list_section">
           <input className="hide" type="checkbox" name="my_basket" value="value" id="not_my_basket_section" />
-          <label htmlFor="not_my_basket_section">
+          <label htmlFor="not_my_basket_section" className="basket_section_label">
             <p>Доступные мне</p>
             <svg className="arrow_down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129">
               <use xlinkHref="#arrow"></use>
