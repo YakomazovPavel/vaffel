bc3413a4028f32e442603281f2dc64901b3e5079
YakomazovPavel <yakomazov.pavel@yandex.ru>
2025-08-19 18:54:28 +0300

    1

--- src/pages/BasketList/BasketList.jsx
+++ src/pages/BasketList/BasketList.jsx
@@ -85,7 +85,6 @@ function BasketList() {
 
   useEffect(() => {
     open();
-    load;
     return close;
   }, []);
 
@@ -118,76 +117,31 @@ function BasketList() {
             ))}
           </div>
         )}
+        {basketListData?.other && (
+          <div className="basket_list_section">
+            <input className="hide" type="checkbox" name="my_basket" value="value" id="not_my_basket_section" />
+            <label htmlFor="not_my_basket_section" className="basket_section_label">
+              <p>Доступные мне</p>
+              <svg className="arrow_down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129">
+                <use xlinkHref="#arrow"></use>
+              </svg>
+            </label>
 
-        <div className="basket_list_section">
-          <input className="hide" type="checkbox" name="my_basket" value="value" id="not_my_basket_section" />
-          <label htmlFor="not_my_basket_section" className="basket_section_label">
-            <p>Доступные мне</p>
-            <svg className="arrow_down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129">
-              <use xlinkHref="#arrow"></use>
-            </svg>
-          </label>
-          <div className="basket_list_item">
-            <img src="4.jpg" />
-            <div>
-              <h1>Проводы</h1>
-              <h2>#12</h2>
-            </div>
-            <button>
-              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                <use xlinkHref="#circle_arrow"></use>
-              </svg>
-            </button>
+            {basketListData.other.map((basket) => (
+              <div className="basket_list_item">
+                <img src={basket.photo_url} />
+                <div>
+                  <h1>{basket.name}</h1>
+                </div>
+                <button>
+                  <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                    <use xlinkHref="#circle_arrow"></use>
+                  </svg>
+                </button>
+              </div>
+            ))}
           </div>
-          <div className="basket_list_item">
-            <img src="5.jpg" />
-            <div>
-              <h1>Свадьба</h1>
-              <h2>#15</h2>
-            </div>
-            <button>
-              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                <use xlinkHref="#circle_arrow"></use>
-              </svg>
-            </button>
-          </div>
-          <div className="basket_list_item">
-            <img src="6.jpg" />
-            <div>
-              <h1>Посиделки 12.05</h1>
-              <h2>#72</h2>
-            </div>
-            <button>
-              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                <use xlinkHref="#circle_arrow"></use>
-              </svg>
-            </button>
-          </div>
-          <div className="basket_list_item">
-            <img src="7.jpg" />
-            <div>
-              <h1>Посиделки маглов</h1>
-              <h2>#82</h2>
-            </div>
-            <button>
-              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                <use xlinkHref="#circle_arrow"></use>
-              </svg>
-            </button>
-          </div>
-          <div className="basket_list_item">
-            <img src="8.jpg" />
-            <div>
-              <h1>Посиделки без повода</h1>
-              <h2>#17</h2>
-            </div>
-            <button>
-              <svg className="arrow_rigth" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                <use xlinkHref="#circle_arrow"></use>
-              </svg>
-            </button>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
