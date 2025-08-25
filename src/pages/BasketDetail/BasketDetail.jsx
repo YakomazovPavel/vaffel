3666fe4defd97a6e440ea319d61bcf6fe74ef82b
YakomazovPavel <yakomazov.pavel@yandex.ru>
2025-08-25 23:50:52 +0300

    1

--- src/pages/BasketDetail/BasketDetail.jsx
+++ src/pages/BasketDetail/BasketDetail.jsx
@@ -71,13 +71,14 @@ function BasketDetail() {
   }, []);
 
   var copyLinkHandler = async () => {
+    Telegram.WebApp.showAlert("Ссылка скопирована");
     // window.Telegram.WebApp.shareMessage(1);
-    if (navigator?.clipboard?.writeText) {
-      // Это работает на телефоне =/
-      await navigator.clipboard.writeText("Рандомный текст в буфере обмена");
-      console.log("Text copied to clipboard successfully!");
-      Telegram.WebApp.showAlert("Ссылка скопирована");
-    }
+    // if (navigator?.clipboard?.writeText) {
+    // Это работает на телефоне =/
+    // await navigator.clipboard.writeText("Рандомный текст в буфере обмена");
+    // console.log("Text copied to clipboard successfully!");
+    Telegram.WebApp.showAlert("Ссылка скопирована");
+    // }
   };
 
   var addItemHandler = () => {
