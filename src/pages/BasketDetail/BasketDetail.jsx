dfe38f8606a347e0e5f837027f75975da44b7c27
YakomazovPavel <yakomazov.pavel@yandex.ru>
2025-08-25 23:09:06 +0300

    1

--- src/pages/BasketDetail/BasketDetail.jsx
+++ src/pages/BasketDetail/BasketDetail.jsx
@@ -99,6 +99,11 @@ function BasketDetail() {
                   <use xlinkHref="#share"></use>
                 </svg>
               </button>
+              <button>
+                <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <use xlinkHref="#share2"></use>
+                </svg>
+              </button>
             </div>
           </div>
 
